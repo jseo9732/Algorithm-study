74383354ec4218bfcce2869714e4ddb06cb9963c
jisu Seo <jseo9732@gmail.com>
2021-12-15 03:44:13 +0900

    3단계 for문

---
+++ 단계별로풀어보기/3. for문/3-8393_합.py
@@ -0,0 +1,6 @@
+n = int(input())
+q = 0
+
+for i in range(1, n+1):
+    q += i
+print(q)
---
+++ 단계별로풀어보기/3. for문/4-15552_빠른 A+B.py
@@ -0,0 +1,6 @@
+import sys
+T = int(sys.stdin.readline())
+
+for t in range(T):
+    A, B = map(int, input().split())
+    print(A + B)
